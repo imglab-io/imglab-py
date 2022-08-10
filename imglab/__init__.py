9ed9ec018e1a6c46251cfcc69e9301685250f1bf
José Francisco Calvo <josefranciscocalvo@gmail.com>
2022-08-10 13:04:49 +0200

    Add package version

--- imglab/__init__.py
+++ imglab/__init__.py
@@ -3,4 +3,8 @@ from .color import color
 from .position import position
 from .url import url
 
+from . import _version
+
+__version__ = _version.version
+
 __all__ = ["Source", "color", "position", "url"]
